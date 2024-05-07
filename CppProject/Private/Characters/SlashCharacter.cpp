e53640dc35651230385902f235eda3a8a73ced15
LaniusWreath <shrike3040@naver.com>
2024-05-08 02:36:45 +0900

    Enhanced Input Action Binding on Character : Move, Look, Jump etc

--- CppProject/Private/Characters/SlashCharacter.cpp
+++ CppProject/Private/Characters/SlashCharacter.cpp
@@ -2,8 +2,11 @@
 
 
 #include "Characters/SlashCharacter.h"
-#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/SpringArmComponent.h" 
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
 ASlashCharacter::ASlashCharacter()
@@ -15,7 +18,7 @@ ASlashCharacter::ASlashCharacter()
 	SpringArm->TargetArmLength = 300.f;
 
 	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
-	ViewCamera->SetupAttachment(GetRootComponent());
+	ViewCamera->SetupAttachment(SpringArm);
 
 }
 
@@ -23,6 +26,13 @@ void ASlashCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(SlashContext, 0);
+		}
+	}
 }
 
 void ASlashCharacter::Tick(float DeltaTime)
@@ -31,13 +41,69 @@ void ASlashCharacter::Tick(float DeltaTime)
 
 }
 
+//EnhancedInputComponent에 IA 입출력 함수 바인딩 함수
 void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	//EnhancedInputComponent가 nullptr인지 확인
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//EnhancedInputComponent에 입력->출력(이동) 함수 바인딩
+		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Move);
 
+		//EnhancedInputComponent에 입력->출력(회전) 함수 바인딩
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Look);
+
+		//EnhancedInputComponent에 Default 점프 함수 바인딩
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Jump);
+		
+		//EnhancedInputComponent에 EKey 입력 함수 바인딩
+		EnhancedInputComponent->BindAction(EKeyAction, ETriggerEvent::Triggered, this, &ASlashCharacter::EKeyPressed);
+
+		//EnhancedInputComponent에 공격 입력 함수 바인딩
+		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Attack);
+
+		//EnhancedInputComponent에 회피 입력 함수 바인딩
+		EnhancedInputComponent->BindAction(DodgeAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Dodge);
+	}
 }
 
 void ASlashCharacter::MoveForward(float Value)
 {
 
+}
+
+//IA 이벤트 출력 함수
+void ASlashCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirrection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirrection, MovementVector.Y);
+	const FVector RightDirrection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirrection, MovementVector.X);
+
+}
+
+void ASlashCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+}
+
+void ASlashCharacter::EKeyPressed(const FInputActionValue& Value)
+{
+}
+
+void ASlashCharacter::Attack(const FInputActionValue& Value)
+{
+}
+
+void ASlashCharacter::Dodge(const FInputActionValue& Value)
+{
 }
--- CppProject/Public/Characters/SlashCharacter.h
+++ CppProject/Public/Characters/SlashCharacter.h
@@ -4,10 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "SlashCharacter.generated.h"
 
 class USpringArmComponent;
 class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+
 
 UCLASS()
 class CPPPROJECT_API ASlashCharacter : public ACharacter
@@ -18,10 +22,18 @@ public:
 	ASlashCharacter();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	//점프 함수(캐릭터 클래스에서 오버라이드, 입력 매개변수 필요 없음, bool)
+	//virtual void Jump() override;
 
 protected:
 	virtual void BeginPlay() override;
 	void MoveForward(float Value);
+	//IA 이벤트 출력 함수
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void EKeyPressed(const FInputActionValue& Value);
+	void Attack(const FInputActionValue& Value);
+	void Dodge(const FInputActionValue& Value);
 
 private:
 	UPROPERTY(VisibleAnywhere)
@@ -29,4 +41,27 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	UCameraComponent* ViewCamera;
+
+	//IMC
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputMappingContext* SlashContext;
+
+	//IA
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* EKeyAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* AttackAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* DodgeAction;
 };
