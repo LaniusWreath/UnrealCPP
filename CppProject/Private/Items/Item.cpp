338ab44676d012027ab886b9fa2b65a1d6454cc0
LaniusWreath <shrike3040@naver.com>
2024-05-31 02:14:20 +0900

    추가 : 무기 콜리전 추가 및 캐릭터 오버랩  델리게이트 설정

--- CppProject/Private/Items/Item.cpp
+++ CppProject/Private/Items/Item.cpp
@@ -3,6 +3,7 @@
 #include "Items/Item.h"
 #include "CppProject/DebugMacros.h"
 #include "CppProject/CppProject.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AItem::AItem()
@@ -10,16 +11,20 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
 	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
 	SetRootComponent(ItemMesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSphereEndOverlap);
 }
 
 float AItem::TransformedSin()
@@ -35,6 +40,26 @@ float AItem::TransformedCos()
 float AItem::RotatedRational()
 {
 	return RotationValue/RunningTime;
+}
+
+void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FString OtherActorName = OtherActor->GetName();
+	//온 스크린 디버그 메세지
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, OtherActorName);
+	}
+}
+
+void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	const FString OtherActorName = FString("Ending Overlap with: ") + OtherActor->GetName();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Blue, OtherActorName);
+	}
 }
 
 // Called every frame
--- CppProject/Public/Items/Item.h
+++ CppProject/Public/Items/Item.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+class USphereComponent;
 
 UCLASS()
 class CPPPROJECT_API AItem : public AActor
@@ -32,7 +33,8 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Rotation Value")
 	float RotationValue = 1.f;
 
-	//-----------------------------------------------------------------------//
+	//-------------------------Function---------------------------//
+
 	UFUNCTION(BlueprintPure)
 	float TransformedSin();
 
@@ -45,12 +47,21 @@ protected:
 	template<typename T>
 	T avg(T first, T second);
 
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 private : 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
 
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
 };
 
 template<typename T>
