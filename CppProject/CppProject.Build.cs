952404633cf8d86e76e414df50cd62b7d86a9440
LaniusWreath <shrike3040@naver.com>
2024-06-01 04:39:25 +0900

    추가 : 아이템 장착 기능, Item상속 Weapon 클래스

--- CppProject/CppProject.Build.cs
+++ CppProject/CppProject.Build.cs
@@ -11,7 +11,7 @@ public class CppProject : ModuleRules
 		//헤더 추가에 필요한 모듈(플러그인)이 빌드 파일에 필요함(문서에 Build가 Engine이 아니면 따로 추가해야 함). 
 		//아래에 추가이후, 에디터 닫고 비주얼스튜디오 프로젝트 컴파일.
 		//!Groom 붙이려면 HairStandsCore와 Niagara 모듈 동시에 추가해야함
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "GameplayTasks" ,"InputCore", "EnhancedInput", "HairStrandsCore", "Niagara"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- CppProject/Public/Items/Item.h
+++ CppProject/Public/Items/Item.h
@@ -33,6 +33,10 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Rotation Value")
 	float RotationValue = 1.f;
 
+	//StaticMesh
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* ItemMesh;
+
 	//-------------------------Function---------------------------//
 
 	UFUNCTION(BlueprintPure)
@@ -48,18 +52,16 @@ protected:
 	T avg(T first, T second);
 
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION()
-	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 private : 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
 
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* ItemMesh;
-
+	
 	UPROPERTY(VisibleAnywhere)
 	USphereComponent* Sphere;
 };
